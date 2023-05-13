5ca0896d7753fc341746fd12dd5fa68ea3ec7db4
Duncan Dean <git@dunxen.dev>
2023-05-13 18:44:49 +0200

    Stylezzz

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'nexed': '#1d0e44',
-        'brutal-orange': '#f19143',
-        'regia': '#ff773d',
+        'nexed': '#1d0e4422',
+        'starlark': '#452a7333',
+        'brutal-orange': '#ffb952',
+        'regia': '#ff5b15',
       },
     },
   },
